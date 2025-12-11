b98e11b02c802974b650b9c8415565311bbbd21d
Amit Chaudhary <ak.chaudhary8125@gmail.com>
2025-12-11 06:44:38 +0545

    learning continued

---
+++ practiceqs1.c
@@ -0,0 +1,11 @@
+// WAP to check if a number is divisible by  2 or not?
+
+#include<stdio.h>
+
+int main (){
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("%d \n", a % 2 == 0);
+    return 0;
+}
---
+++ type_conversion.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("%f \n", 2.0 * 2);
+    return 0;
+}
