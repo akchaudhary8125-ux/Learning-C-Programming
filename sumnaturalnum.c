20575b2def4694efc072b3b88431c6b96842e9be
Amit Chaudhary <ak.chaudhary8125@gmail.com>
2025-12-15 09:17:23 +0545

    learning continued

--- sumnaturalnum.c
+++ sumnaturalnum.c
@@ -6,8 +6,9 @@ int main() {
     scanf("%d", &n);
 
     int sum = 0;
-    for (int i=1; i<=n; i++){
+    for (int i=1, j=n; i<=n && j>=1; i++,j--){
         sum += i;
+        printf("%d\n", j);
     }
 
     printf("The sum of first %d natural numbers is %d\n", n , sum);
