5f191bc26bf78b06d3a824ebe66bb012e90dfdcc
Amit Chaudhary <ak.chaudhary8125@gmail.com>
2025-12-17 20:05:32 +0545

    learning continued

---
+++ nthfibonacci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int fib(int n);
+
+int main () {
+    int nth;
+    printf("Enter the nth term to print fibonacci number:");
+    scanf("%d", &nth);
+    printf("The fibonacci number of %dth term is %d", nth, fib(nth));
+    return 0;
+}
+
+int fib(int n) {
+    if (n==0) {
+        return 0;
+    }
+    if (n==1){
+        return 1;
+    }
+    int fibNm1 = fib(n-1);
+    int fibNm2 = fib(n-2);
+    int fibN = fibNm1 + fibNm2;
+    return fibN;
+}
